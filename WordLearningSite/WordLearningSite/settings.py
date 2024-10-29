7514a33a3b2cc4967b2734dc01647f46696699ea
JTNewY <niner2@naver.com>
2024-10-29 17:49:07 +0900

    동기화2

--- WordLearningSite/WordLearningSite/settings.py
+++ WordLearningSite/WordLearningSite/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 STATICFILES_DIRS = [
     BASE_DIR / "words" / "static",
 ]
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
